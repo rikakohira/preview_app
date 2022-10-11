0e2441aa9d6e971a7b2d84be6194a54f6d18b0f4
rikakohira <rikako.takase88@gmail.com>
2022-10-11 15:46:11 +0900

    プレビュー追加

---
+++ app/javascript/preview.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // フォームを取得
+  const postForm = document.getElementById('new_post');
+  // プレビューを取得
+  const previewList = document.getElementById('previews');
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+
+
+});
