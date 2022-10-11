3e8610de329abb1ec9232be57889e91a41e78a6a
rikakohira <rikako.takase88@gmail.com>
2022-10-11 16:53:18 +0900

    プレビュー表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -6,5 +6,22 @@ document.addEventListener('DOMContentLoaded', function(){
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
 
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+  // ファイル選択時にイベント発火
+  fileField.addEventListener('change', function(e){
+    const file = e.target.files[0];
+    // 画像情報を渡してURLを生成
+    const blob = window.URL.createObjectURL(file);
+    // 画像を表示するHTMLを作成
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
 
+    // 生成したHTMLの要素をブラウザに表示させる
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
 });
